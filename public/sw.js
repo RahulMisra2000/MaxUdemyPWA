7f02f8f6c8c1a93155ece8b0c26b63fb7d3cd317
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-07-14 20:44:54 -0400

    Caching strategy based on the URL

--- public/sw.js
+++ public/sw.js
@@ -129,59 +129,66 @@ self.addEventListener('fetch', function(event) {
             .catch(function(err) {
 >>>>>>> fc78a00c5d90e9ffdfb370ae10b94e3f7fc3d877
 
-self.addEventListener('fetch', function (event) {
 
+            
+  self.addEventListener('fetch', function (event) {
+    
+  /* ********** CACHE STRATEGY BASED ON THE URL ******************************************************************************* */
   var url = 'https://pwagram-99adf.firebaseio.com/posts';
-  if (event.request.url.indexOf(url) > -1) {
-    event.respondWith(fetch(event.request)
-      .then(function (res) {
-        var clonedRes = res.clone();
-        clearAllData('posts')
-          .then(function () {
-            return clonedRes.json();
-          })
-          .then(function (data) {
-            for (var key in data) {
-              writeData('posts', data[key])
-            }
-          });
-        return res;
-      })
-    );
-  } else if (isInArray(event.request.url, STATIC_FILES)) {
-    event.respondWith(
-      caches.match(event.request)
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function (response) {
-          if (response) {
-            return response;
-          } else {
-            return fetch(event.request)
+  if (event.request.url.indexOf(url) > -1) {                      // based on the url ******************
+            event.respondWith(fetch(event.request)
               .then(function (res) {
-                return caches.open(CACHE_DYNAMIC_NAME)
-                  .then(function (cache) {
-                    // trimCache(CACHE_DYNAMIC_NAME, 3);
-                    cache.put(event.request.url, res.clone());
-                    return res;
+                var clonedRes = res.clone();
+                clearAllData('posts')
+                  .then(function () {
+                    return clonedRes.json();
                   })
-              })
-              .catch(function (err) {
-                return caches.open(CACHE_STATIC_NAME)
-                  .then(function (cache) {
-                    if (event.request.headers.get('accept').includes('text/html')) {
-                      return cache.match('/offline.html');
+                  .then(function (data) {
+                    for (var key in data) {
+                      writeData('posts', data[key])
                     }
                   });
-              });
-          }
-        })
-    );
-  }
+                return res;
+              })
+            );
+  } else if (isInArray(event.request.url, STATIC_FILES)) {        // based on the url ******************
+            event.respondWith(
+              caches.match(event.request)
+            );
+  } else {                                                       // based on the url ******************
+            event.respondWith(
+              caches.match(event.request)
+                .then(function (response) {
+                  if (response) {
+                     return response;
+                  } else {
+                    return fetch(event.request)
+                      .then(function (res) {
+                        return caches.open(CACHE_DYNAMIC_NAME)
+                          .then(function (cache) {
+                            // trimCache(CACHE_DYNAMIC_NAME, 3);
+                            cache.put(event.request.url, res.clone());
+                            return res;
+                          })
+                      })
+                      .catch(function (err) {
+                        return caches.open(CACHE_STATIC_NAME)
+                          .then(function (cache) {
+                            if (event.request.headers.get('accept').includes('text/html')) {
+                              return cache.match('/offline.html');
+                            }
+                          });
+                      });
+                  }
+                })
+            );
+    }
 });
+/* ********** CACHE STRATEGY BASED ON THE URL ******************************************************************************* */
 
+            
+            
+            
 // self.addEventListener('fetch', function(event) {
 //   event.respondWith(
 //     caches.match(event.request)
